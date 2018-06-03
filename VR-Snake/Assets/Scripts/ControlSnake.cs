f366c4f72c1b335de7e7843d25f0fd56b334cfe4
prutoplox <marius.hackenberg@hotmail.de>
2018-06-03 15:35:56 +0200

    firstCommit
    First Commit

---
+++ VR-Snake/Assets/Scripts/ControlSnake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSnake : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("up");
+            MovementSnake.instance.rotateUp();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("down");
+            MovementSnake.instance.rotateDown();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("left");
+            MovementSnake.instance.rotateLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("right");
+            MovementSnake.instance.rotateRight();
+        }
+    }
+}
---
+++ VR-Snake/Assets/Scripts/CreateMap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMap : MonoBehaviour {
+
+    public Transform startSphere;
+    public GameObject sphere;
+    public int size;
+    public float scalefactor;
+
+	// Use this for initialization
+	void Start () {
+        scalefactor = 1;
+
+        for (int i = 0; i <= size; i++)
+        {
+            for (int j = 0; j <= size; j++)
+            {
+                for(int k = 0; k <= size; k++)
+                {
+                    //Debug.Log(k.ToString() + i.ToString() + j.ToString());
+                    Instantiate(sphere, startSphere.transform.position + new Vector3(k*scalefactor,j*scalefactor,i*scalefactor) , startSphere.rotation);
+                }   
+            }
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
