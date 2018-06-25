2db9a7f6797fc82f721975f75f07cb971f2b74c4
prutoplox <marius.hackenberg@hotmail.de>
2018-06-25 14:29:33 +0200

    Add Gaze to snake for better orientation

---
+++ VR_Snake/Assets/Scripts/SnakeGaze.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeGaze : MonoBehaviour
+{
+    private Vector3[] values = new Vector3[2];
+    public LineRenderer line;
+    public Camera cam;
+    private Ray hitInfo;
+    private Vector3 headPosition;
+    private Quaternion headRotation;
+    private Vector3 gazeDirection;
+
+    private Vector3 oldOffset = new Vector3(0, 0, 0);
+
+    void Start()
+    {
+        line.positionCount = 2;
+    }
+
+    void Update()
+    {
+        transform.rotation = cam.transform.rotation;
+        transform.position = cam.transform.position;
+        transform.Translate(Vector3.forward * 30);
+        headPosition = cam.gameObject.transform.position;
+        headPosition.y = headPosition.y - 0.25f;
+        values[0] = headPosition;
+        values[1] = transform.position;
+        line.SetPositions(values);
+        //Get position of the TestCameraParent and move it to the old location to avoid drifting
+        Vector3 parentPosition = cam.transform.parent.position - oldOffset;
+        //Get position of the TestCamera
+        Vector3 camOffset = cam.transform.position;
+
+        oldOffset = camOffset;
+        //Substract the postition of the TestCamera relative to the parent to reset it where it should be
+        cam.transform.parent.position = parentPosition - camOffset;
+
+    }
+}
