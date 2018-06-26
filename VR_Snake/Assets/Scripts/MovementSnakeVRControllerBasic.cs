d07690bfd012e45c5a9e81484ee5ae3ded137355
prutoplox <marius.hackenberg@hotmail.de>
2018-06-26 14:02:07 +0200

    Snake controller VR

---
+++ VR_Snake/Assets/Scripts/MovementSnakeVRControllerBasic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.WSA;
+
+public class MovementSnakeVRControllerBasic : MonoBehaviour {
+    
+    public string[] cnames;
+
+    private void Start()
+    {
+        cnames = Input.GetJoystickNames();
+    }
+
+    public void Update()
+    {
+        if (VariableManager.instance.useVRControllerBasic)
+        {
+                changeDirection();
+        }
+    }
+
+    private void changeDirection()
+    {
+        if (Input.GetAxis("Vertical") > 0)  
+        {
+            Debug.Log("+");
+            MovementSnake.instance.rotateUp();
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            Debug.Log("-");
+            MovementSnake.instance.rotateDown();
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            Debug.Log("+");
+            MovementSnake.instance.rotateRight();
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            Debug.Log("-");
+            MovementSnake.instance.rotateLeft();
+        }
+    }
+}
---
+++ VR_Snake/Assets/Scripts/MovementVRControllerButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementVRControllerButtons : MonoBehaviour {
+
+    public void Update()
+    {
+        if (VariableManager.instance.useVRControllerBasicButtons)
+        {
+            useAction();
+        }
+    }
+
+    private void useAction()
+    {
+        if (Input.GetButtonDown(KeyCode.Joystick1Button14.ToString()))
+        {
+            Debug.Log("action1");
+        }
+        else if (Input.GetButtonDown(KeyCode.Joystick1Button4.ToString()))
+        {
+            Debug.Log("action2");
+        }
+    }
+}
