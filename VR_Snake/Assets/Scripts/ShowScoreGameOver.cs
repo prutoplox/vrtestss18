6b67be5054df80dd879441f9f531a98a65603637
prutoplox <marius.hackenberg@hotmail.de>
2018-09-25 11:58:03 +0200

    scoregameover

---
+++ VR_Snake/Assets/Scripts/ShowScoreGameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScoreGameOver : MonoBehaviour {
+
+    public Text scoreText;
+    public Text scoreTextS;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (VariableManager.instance.showGameOver){
+            scoreText.text = VariableManager.instance.score.ToString();
+            scoreText.text = VariableManager.instance.score.ToString();
+        }
+	}
+}
