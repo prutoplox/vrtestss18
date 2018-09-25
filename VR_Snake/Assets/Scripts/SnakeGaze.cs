823c6bc87bdebd1ea42a87d85883ebecbc182cfd
prutoplox <marius.hackenberg@hotmail.de>
2018-09-25 08:15:22 +0200

    Add gaze menu

--- VR_Snake/Assets/Scripts/SnakeGaze.cs
+++ VR_Snake/Assets/Scripts/SnakeGaze.cs
@@ -13,7 +13,6 @@ public class SnakeGaze : MonoBehaviour
     private Quaternion headRotation;
     private Vector3 gazeDirection;
 
-
     private Vector3 oldOffset = new Vector3(0, 0, 0);
 
     void Start()
@@ -31,14 +30,16 @@ public class SnakeGaze : MonoBehaviour
         values[0] = headPosition;
         values[1] = transform.position;
         line.SetPositions(values);
+
         //Get position of the TestCameraParent and move it to the old location to avoid drifting
         Vector3 parentPosition = cam.transform.parent.position - oldOffset;
+
         //Get position of the TestCamera
         Vector3 camOffset = cam.transform.position;
 
         oldOffset = camOffset;
+
         //Substract the postition of the TestCamera relative to the parent to reset it where it should be
         cam.transform.parent.position = parentPosition - camOffset;
-
     }
 }
