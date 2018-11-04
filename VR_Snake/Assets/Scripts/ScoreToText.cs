1652fe5aa35b411ca86f40c7b4ba1373435fcf4b
prutoplox <marius.hackenberg@hotmail.de>
2018-11-04 16:25:50 +0100

    Add Score to gui

---
+++ VR_Snake/Assets/Scripts/ScoreToText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class ScoreToText : MonoBehaviour {
+    public Text scoreT;
+    public Text scoreS;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (VariableManager.instance.showMainMenu)
+        {
+           scoreT.enabled = false;
+           scoreS.enabled = false;
+        }
+        else
+        {
+            scoreT.enabled = true;
+            scoreS.enabled = true;
+            scoreT.text = VariableManager.instance.score.ToString("F2");
+            scoreS.text = VariableManager.instance.score.ToString("F2");
+        }
+	}
+}
